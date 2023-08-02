22bf226cdb5452b254e8f377ad3d86d511cdfcea
Bill Richards <bill@greyskin.co.uk>
2023-08-02 13:46:44 +0100

    documentation for gssdc_iic_master

--- components/iic/include/gsdc_iic_master.h
+++ components/iic/include/gsdc_iic_master.h
@@ -6,7 +6,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+/**
+ * @brief Create and run the task for the Master IIC device. The task will poll all configured IIC clients on the IIC Bus, and return all data received.
+ * @param iic_configuration an instantiated gsdc_iic_configuration_t
+ */
 void gsdc_iic_master_task_create(gsdc_iic_configuration_t * iic_configuration);
 
 #ifdef __cplusplus
